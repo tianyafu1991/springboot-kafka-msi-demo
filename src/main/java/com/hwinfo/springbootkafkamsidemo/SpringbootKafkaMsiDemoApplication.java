c6827ac4187d14ab1acf40da0fe02b47e97cb268
tianyafu <1193835093@qq.com>
2019-07-07 19:51:46 +0800

    init:init project

---
+++ src/main/java/com/hwinfo/springbootkafkamsidemo/SpringbootKafkaMsiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hwinfo.springbootkafkamsidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKafkaMsiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootKafkaMsiDemoApplication.class, args);
+    }
+
+}
